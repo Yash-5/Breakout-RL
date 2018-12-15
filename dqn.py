53a2497c8dc5ec4c993f42736f6f5682bdf07af6
Yash2208 <cs14btech11040@iith.ac.in>
2018-12-14 19:46:48 -0500

    some preprocessing added

--- dqn.py
+++ dqn.py
@@ -8,6 +8,15 @@ import tensorflow as tf
 
 from collections import deque, namedtuple
 
+class state_processor():
+    def __init__(self, input_size):
+        with tf.variable_scope("state_processor"):
+            self.input_state = tf.placeholder(shape=input_size, dtype=tf.uint8)
+            self.output = tf.squeeze(tf.image.rgb_to_grayscale(self.input_state))
+
+	def process(self, sess, inpu_state):
+        return sess.run(self.output, feed_dict={self.input_state: input_state})
+
 def main():
     env = gym.make("Breakout-v0")
 
