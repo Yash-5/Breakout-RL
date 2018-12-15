6e40d1201a2865ec917de221c5beb07d44725dec
Yash2208 <cs14btech11040@iith.ac.in>
2018-12-14 19:30:54 -0500

    env

---
+++ dqn.py
@@ -0,0 +1,15 @@
+import gym
+import itertools
+import numpy as np
+import os
+import random
+import sys
+import tensorflow as tf
+
+from collections import deque, namedtuple
+
+def main():
+    env = gym.make("Breakout-v0")
+
+if __name__ == '__main__':
+    main()
